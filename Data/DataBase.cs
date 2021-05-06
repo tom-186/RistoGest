7f1ed40cfa84f9c7fa0b2b978ed947882915ec88
tom-186 <tommaso.manni@gmail.com>
2021-05-06 13:32:23 +0200

    Upload project files

---
+++ Data/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RistoGest.Data
+{
+    public class DataBase : DbContext
+    {
+        public DbSet<Piatto> Piatti { get; set; }
+
+        public DataBase(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ Data/Piatto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RistoGest.Data
+{
+    public class Piatto
+    {
+        public int Id { get; set; }
+        public string Tipo_portata { get; set; }
+        public string Nome { get; set; }
+        public string Allergeni { get; set; }
+        public string Stagione { get; set; }
+        public decimal Prezzo { get; set; }
+    }
+}
